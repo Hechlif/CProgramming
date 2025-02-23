4746cd1901c8b4d8f2c77e904bb20fa48b401107
Hechlif <joao.mclfs@gmail.com>
2025-02-23 20:45:21 +0000

    first commit

---
+++ aula1/Aula1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int var = 1;
+    printf("Hello World\n");
+    if(var ==1){
+        printf("Var é igual a 1\n");
+    }
+    return;
+}
