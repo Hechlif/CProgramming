0f737b5bd35345ae73e1c16c41accea91c86a712
GONGUINI <goncalo.lima.guimaraes@gmail.com>
2025-02-25 16:57:26 +0000

    CARALHO

--- aula2/problemas da aula/problemaaula2.c
+++ aula2/problemas da aula/problemaaula2.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(){
-    int a;
-    float n;
+int main() {
+    double numero;
+    int n, potencia = 1;
 
-    printf("Digite um numero: ");
-    scanf("%f", &n);
-    
-    printf("Quer ter quantas casas décimais? (1 - 7) : ");
-    scanf("%d", &a);
-
-    while (a > 0)
-    {
-        n = n * (1/10);
-        a = a - 1;
+    printf("Digite um número real positivo: ");
+    if (scanf("%lf", &numero) != 1 || numero <= 0) {
+        printf("Entrada inválida. Digite ser um número real positivo.\n");
+        return 1;
     }
 
-    printf("%f", n);
+    printf("Digite um valor para n (1 a 7): ");
+    if (scanf("%d", &n) != 1 || n < 1 || n > 7) {
+        printf("Entrada inválida. n deve estar entre 1 e 7.\n");
+        return 1;
+    }
+
+     for (int i = 0; i < n; i++) {
+        potencia *= 10;
+    }
+
+    int numero_arredondado = (int) (numero / potencia + 0.5) * potencia;
+
+    printf("Número arredondado: %d\n", numero_arredondado);
+
+    return 0;
 }
