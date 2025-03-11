1bd8fc7a886f07f76bf350733776e465431ebaeb
GONGUINI <goncalo.lima.guimaraes@gmail.com>
2025-03-11 16:52:29 +0000

    agora tem %

--- Aulas_laboratorio/aula3-4/Problemas_aula_2/ex1.c
+++ Aulas_laboratorio/aula3-4/Problemas_aula_2/ex1.c
@@ -1,16 +1,10 @@
-// Escrever um programa em C que faça contas de divisao inteira, multiplicaçao sobre os restos de divisaoes. Use a
-// função rand() para gerar dois inteiros entre 1 e 100, que são mostrados ao aluno. O aluno deverá introduzir o
-// resultado e este deverá ser comparado com a solução correta. O programa deverá permitir que o aluno faça várias
-// tentativas, até acertar, e em cada tentativa indicar se o aluno acertou ou errou. O programa apenas deve sair
-// quando o aluno introduzir o número menos um.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main() {
-    int num1, num2, resultado, resposta, opcao;
-    srand(time(NULL));
+    int num1, num2, resultado, resposta, opcao, total_perguntas = 0, respostas_corretas = 0;
+    srand(time(NULL)); 
 
     while (1) {
         opcao = rand() % 3 + 1;
@@ -23,7 +17,7 @@ int main() {
                 printf("Quanto é %d multiplicado por %d?\n", num1, num2);
                 resultado = num1 * num2;
                 break;
-            case 2:
+            case 2: 
                 while (num1 % num2 != 0) {
                     num1 = rand() % 100 + 1;
                     num2 = rand() % 100 + 1;
@@ -31,23 +25,27 @@ int main() {
                 printf("Quanto é %d dividido por %d?\n", num1, num2);
                 resultado = num1 / num2;
                 break;
-            case 3:
+            case 3: 
                 printf("Qual é o resto da divisão de %d por %d?\n", num1, num2);
                 resultado = num1 % num2;
                 break;
         }
+
+        total_perguntas++;
 
         while (1) {
             printf("Sua resposta (-1 para sair): ");
             scanf("%d", &resposta);
             
             if (resposta == -1) {
-                printf("Programa encerrado.\n");
+                float percentagem = (total_perguntas == 0) ? 0 : ((float)respostas_corretas / total_perguntas) * 100;
+                printf("Programa encerrado. Você acertou %d de %d perguntas (%.2f%%).\n", respostas_corretas, total_perguntas, percentagem);
                 return 0;
             }
             
             if (resposta == resultado) {
                 printf("Correto! Vamos para outra conta.\n\n");
+                respostas_corretas++;
                 break;
             } else {
                 printf("Errado! Tente novamente.\n");
