4423c512279c8586dcee4151c571e327276de476
Hechlif <joao.mclfs@gmail.com>
2025-03-14 21:05:56 +0000

    vou tentar tirar o malloc e fazer com que funcione na mesma

--- projeto/projeto_sem_alloc/projeto_sem_alloc.c
+++ projeto/projeto_sem_alloc/projeto_sem_alloc.c
@@ -12,39 +12,34 @@ typedef struct {
     size_t count;
 } dicionario;
 
-dicionario *load_dicionario(const char *filename) {
-    printf("A fazer loading do dicionário através do ficheiro: %s\n", filename);
-    FILE *file = fopen(filename, "r");
-    if (!file) {
+dicionario *load_dicionario(const char *fname) {
+    printf("A fazer loading do dicionário através do ficheiro: %s\n", fname);
+    FILE *f = fopen(fname, "r");
+    if (!f) {
         perror("Erro ao abrir o dicionário");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
     
-    dicionario *dict = malloc(sizeof(dicionario));
-    if (!dict) {
+    dicionario *d = malloc(sizeof(dicionario));
+    if (!d) {
         perror("Erro ao alocar memória para o dicionário");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
-    dict->count = 0;
+    d->count = 0;
     
-    while (dict->count < MAXIMO_PALAVRAS && fgets(dict->palavras[dict->count], MAXIMO_TAMANHO_DE_PALAVRAS, file)) {
-        dict->palavras[dict->count][strcspn(dict->palavras[dict->count], "\n")] = '\0';
-        printf("Palavra carregada: %s\n", dict->palavras[dict->count]);
-        dict->count++;
+    while (d->count < MAXIMO_PALAVRAS && fgets(d->palavras[d->count], MAXIMO_TAMANHO_DE_PALAVRAS, f)) {
+        d->palavras[d->count][strcspn(d->palavras[d->count], "\n")] = '\0';
+        d->count++;
     }
-    fclose(file);
+    fclose(f);
     
-    printf("Dicionário carregado com %zu palavras\n", dict->count);
-    return dict;
+    printf("Dicionário carregado com %zu palavras\n", d->count);
+    return d;
 }
 
-int compare_words(const void *a, const void *b) {
-    return strcasecmp(*(const char **)a, *(const char **)b);
-}
-
-int palavra_no_dicionario(dicionario *dict, const char *word) {
-    for (size_t i = 0; i < dict->count; i++) {
-        if (strcasecmp(dict->palavras[i], word) == 0) {
+int palavra_no_dicionario(dicionario *d, const char *word) {
+    for (size_t i = 0; i < d->count; i++) {
+        if (strcasecmp(d->palavras[i], word) == 0) {
             printf("\"%s\" foi encontrado no dicionário\n", word);
             return 1;
         }
@@ -53,7 +48,7 @@ int palavra_no_dicionario(dicionario *dict, const char *word) {
     return 0;
 }
 
-void process_text(dicionario *dict) {
+void process_text(dicionario *d) {
     char line[MAXIMO_TAMANHO_DE_PALAVRAS];
     int line_number = 0;
     while (fgets(line, sizeof(line), stdin)) {
@@ -61,7 +56,7 @@ void process_text(dicionario *dict) {
         char *token = strtok(line, " \t.,!?\"'()\n");
         int has_error = 0;
         while (token) {
-            if (!palavra_no_dicionario(dict, token)) {
+            if (!palavra_no_dicionario(d, token)) {
                 if (!has_error) {
                     printf("%d: %s \n", line_number, line);
                     has_error = 1;
@@ -74,13 +69,13 @@ void process_text(dicionario *dict) {
 }
 
 int main(int argc, char *argv[]) {
-    const char *dict_file = "words";
+    const char *d_file = "words";
     if (argc > 1) {
-        dict_file = argv[1];
+        d_file = argv[1];
     }
-    dicionario *dict = load_dicionario(dict_file);
-    process_text(dict);
-    free(dict);
+    dicionario *d = load_dicionario(d_file);
+    process_text(d);
+    free(d);
     printf("Program finished\n");
     
     return EXIT_SUCCESS;
